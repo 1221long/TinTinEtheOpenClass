43849a8a72716fcd4e6db905a4ed375e6b6f9376
1221long <1221long@163.com>
2024-09-19 10:39:29 +0800

    modity insertionSort.sol

--- contracts/t_InsertionSort.sol
+++ contracts/t_InsertionSort.sol
@@ -6,15 +6,27 @@ contract InsertSort {
 
     function resortUintArray(uint[] memory inArray) public pure returns (uint[] memory) {
         uint[] memory outArray;
-
         uint l = inArray.length;
 
         for(uint i = 0; i < l; i++) {
-            outArray[i] = inArray[i];
+            outArray[i] = 0;
             for(uint j = 0; j < l; j++) {
-                if(outArray[i] > inArray[j] && (i == 0 || inArray[j] > outArray[i-1])) {
+                if(i == 0) {
                     outArray[i] = inArray[j];
-                }
+                    if(outArray[i] > inArray[j]) {
+                        outArray[i] = inArray[j];
+                    }
+                } else {
+                    if(inArray[j] > outArray[i-1]) {
+                        if (outArray[i] == 0) {
+                            outArray[i] = inArray[j];
+                        } else {
+                            if(outArray[i] > inArray[j]) {
+                                outArray[i] = inArray[j];
+                            }
+                        }
+                    }
+                }                
             }
         }
 
