94513052eac6d28d3329093e07a3743537097f21
1221long <1221long@163.com>
2024-09-19 16:07:50 +0800

    fix insertsord.sol bug.

--- contracts/t_InsertionSort.sol
+++ contracts/t_InsertionSort.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.8.2 <0.9.0;
 contract InsertSort {
 
     function resortUintArray(uint[] memory inArray) public pure returns (uint[] memory) {
-        uint[] memory outArray;
+        uint[] memory outArray = new uint[](inArray.length);
         uint l = inArray.length;
 
         for(uint i = 0; i < l; i++) {
